b5d465f0e7aeca7e7a94a6271380bf267fa40eb5
HarenaAriel <arielharena@gmail.com>
2021-08-26 15:42:39 +0300

    Test Ajax : envoi et recuperation de donnees

--- WebAppAjax/Controllers/HomeController.cs
+++ WebAppAjax/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebAppAjax.Models;
 
 namespace WebAppAjax.Controllers
 {
   public class HomeController : Controller
   {
+    public List<Employee> employees = new List<Employee>();
+
     public ActionResult Index()
     {
       return View();
@@ -25,6 +28,61 @@ namespace WebAppAjax.Controllers
       ViewBag.Message = "Your contact page.";
 
       return View();
+    }
+
+    [HttpGet]
+    public JsonResult EmployDetailsGet()
+    {
+      try
+      {
+        employees.Add(new Employee {EmployeeId = 1, EmployeeName = "Harena", MapCity = "Antananarivo", MapAddress = "Lot 1"});
+        employees.Add(new Employee {EmployeeId = 2, EmployeeName = "Johan", MapCity = "Antananarivo", MapAddress = "Lot 2"});
+        employees.Add(new Employee {EmployeeId = 3, EmployeeName = "Kianja", MapCity = "Ambatondrazaka", MapAddress = "Lot 3"});
+        employees.Add(new Employee {EmployeeId = 4, EmployeeName = "Tsiry", MapCity = "Antananarivo", MapAddress = "Lot 4"});
+        employees.Add(new Employee {EmployeeId = 5, EmployeeName = "Rija", MapCity = "Ambatondrazaka", MapAddress = "Lot 5"});
+        employees.Add(new Employee {EmployeeId = 6, EmployeeName = "Lahatra", MapCity = "Ambatondrazaka", MapAddress = "Lot 6"});
+
+        return Json(employees, JsonRequestBehavior.AllowGet);
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.ToString());
+        return null;
+      }
+    }
+
+    [HttpPost]
+    public JsonResult EmployDetailsPost() 
+    {
+      try
+      {
+        employees.Add(new Employee { EmployeeId = 1, EmployeeName = "Harena", MapCity = "Antananarivo", MapAddress = "Lot 1" });
+        employees.Add(new Employee { EmployeeId = 2, EmployeeName = "Johan", MapCity = "Antananarivo", MapAddress = "Lot 2" });
+        employees.Add(new Employee { EmployeeId = 3, EmployeeName = "Kianja", MapCity = "Ambatondrazaka", MapAddress = "Lot 3" });
+        employees.Add(new Employee { EmployeeId = 4, EmployeeName = "Tsiry", MapCity = "Antananarivo", MapAddress = "Lot 4" });
+        employees.Add(new Employee { EmployeeId = 5, EmployeeName = "Rija", MapCity = "Ambatondrazaka", MapAddress = "Lot 5" });
+        employees.Add(new Employee { EmployeeId = 6, EmployeeName = "Lahatra", MapCity = "Ambatondrazaka", MapAddress = "Lot 6" });
+
+        return Json(employees, JsonRequestBehavior.AllowGet);
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.ToString());
+        return null;
+      }
+    }
+
+    [HttpPost]
+    public void AddEmploye(int id, string name, string city, string address)
+    {
+      try
+      {
+        employees.Add(new Employee { EmployeeId = id, EmployeeName = name, MapCity = city, MapAddress = address });
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.ToString());
+      }
     }
   }
 }
